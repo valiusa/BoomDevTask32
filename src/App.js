24bba58454ed32157ae3745020e3fb7280104507
Valentin Vladimirov <valentinvladimirov@abv.bg>
2022-12-21 21:48:35 +0200

    task-100 done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,36 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="wrapper">
-        <h3 className="mealHeading">Sushi</h3>
-        <div className="description">I prefer steak than sushi.</div>
-        <div className="price">$9.99</div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          margin: "1rem",
+          paddingBottom: "1rem",
+          borderBottom: "1px solid #ccc",
+        }}
+      >
+        <h3 style={{ margin: "0 0 0.25rem 0" }}>Sushi</h3>
+        <div style={{ fontStyle: "italic" }}>I prefer steak than sushi.</div>
+        <div
+          style={{
+            marginTop: "0.25rem",
+            fontWeight: "bold",
+            color: "#ad5502",
+            fontSize: "1.25rem",
+          }}
+        >
+          $9.99
+        </div>
       </div>
     </div>
+    // <div className="App">
+    //   <div className="wrapper">
+    //     <h3 className="mealHeading">Sushi</h3>
+    //     <div className="description">I prefer steak than sushi.</div>
+    //     <div className="price">$9.99</div>
+    //   </div>
+    // </div>
   );
 }
 
